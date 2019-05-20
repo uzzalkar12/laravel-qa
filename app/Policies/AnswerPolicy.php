aeb52300f8c14a90c86ac4a4c6b994105dc7347e
Uzzal Kar <uzzalkar12@gmail.com>
2019-05-20 14:45:36 +0600

    accept answer

--- app/Policies/AnswerPolicy.php
+++ app/Policies/AnswerPolicy.php
@@ -31,4 +31,9 @@ class AnswerPolicy
     {
         return $user->id === $answer->user_id;
     }
+
+    public function accept(User $user, Answer $answer)
+    {
+        return $user->id === $answer->question->user_id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::get('/questions/{slug}', 'QuestionController@show')->name('questions.show
 
 // Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
